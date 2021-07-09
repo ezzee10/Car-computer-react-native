44dcd0a547234677164fa74daa41137e597da065
ezzee10 <ezequiel.colombano@gmail.com>
2021-07-09 14:09:21 -0300

    Se movió la estructura de app a router y se implementó redux thunk para redux asíncrono

--- config/tokenAuth.js
+++ config/tokenAuth.js
@@ -2,9 +2,7 @@ import { clienteAxios } from './config';
 
 const tokenAuth = token => {
     if (token) {
-        console.log('Entro aca?');
         clienteAxios.defaults.headers.common['x-auth-token'] = token;
-        console.log(clienteAxios.defaults);
     } else {
         delete clienteAxios.defaults.headers.common['x-auth-token'];
     }
